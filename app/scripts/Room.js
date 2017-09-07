6e961c065bb25ac1ead73c7b69b2d6de2c3b6f3b
summerlove91 <gkdus3040@gmail.com>
2017-09-06 23:39:39 -0700

    sample

--- app/scripts/Room.js
+++ app/scripts/Room.js
@@ -1,19 +1,19 @@
 (function () {
   function Room($firebaseArray) {
-      console.log("factory working")
+    console.log("factory working")
     
     var Room = {};
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-      
+
     Room.all = rooms;
 //    rooms.$add({name: "test"})
-    
+
     Room.add = function(room) {
         rooms.$add(room)
      };
- 
-    
+
+
     return Room;
   }
 
